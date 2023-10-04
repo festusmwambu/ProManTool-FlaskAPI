ab6a384790b4b5a312b50b5a7876f5482c652cc4
root <root@DESKTOP-BGD4OC4>
2023-10-04 16:05:51 +0300

    Added .flaskenv module and modified .gitignore

--- app/config.py
+++ app/config.py
@@ -8,7 +8,7 @@ class Config:
     CORS_HEADERS="Content-Type"
     SECRET_KEY=os.environ.get("SECRET_KEY")
     SQLALCHEMY_TRACK_MODIFICATIONS=False
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///promantooldev")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///promantooldev.db")
 
 # `ProductionConfig` class inherits from `Config` base class and sets the `SQLALCHEMY_DATABASE_URI` from the `DATABASE_URL` environment variable, which is suitable for production environment.
 class ProductionConfig(Config):
@@ -17,7 +17,7 @@ class ProductionConfig(Config):
 # `DevelopmentConfig` class inherits from `Config` base class and sets a default `SQLALCHEMY_DATABASE_URI` suitable for development environment 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///promantooldev")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///promantooldev.db")
 # `TestingConfig` class inherits from `Config` base class and sets a default `SQLALCHEMY_DATABASE_URI` suitable for testing environment 
 class TestingConfig(Config):
     TESTING = True
